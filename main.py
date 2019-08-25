3088923ed5a6710bd9735fd2e3747e1ec1ab5f4b
Alfath <alfath.khadijah@gmail.com>
2019-08-25 20:43:32 +0700

    Mencari bilangan terbesar

---
+++ main.py
@@ -0,0 +1,18 @@
+print ('Kakak Cantik')
+
+a = 100
+b = 200
+c = 9999999
+
+
+print(a, b, c)
+
+# gunakan 1 bilangan untuk dicek, misal mulai dari a
+if a > b and a > c:
+    print('a terbesar, nilainya', a)
+
+if b > a and b > c:
+    print('b terbesar, nilainya', b)
+
+if c > a and c > b:
+    print('c terbesar, nilainya', c)
