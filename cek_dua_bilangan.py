6c3e2c3240d7fc245f118091be5d15170baa19ea
Alfath <alfath.khadijah@gmail.com>
2019-09-01 19:56:26 +0700

    Mencari bilangan terbesar

---
+++ cek_dua_bilangan.py
@@ -0,0 +1,15 @@
+a = 2
+b = 2
+
+print(a, b)
+
+#gunakan 1 bilangan untuk dicek, misal mulai dari a
+
+if a > b:
+    print('a terbesar, nilainya', a)
+
+if b > a:
+    print('b terbesar, nilainya', b)
+
+if a == b:
+    print('a dan b sama, nilainya', a)
